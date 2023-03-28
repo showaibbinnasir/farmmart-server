b7f50a3be6747488b47d87fba72a9b02ab673520
showaibbinnasir <showaibnishad@gmail.com>
2023-03-28 01:41:11 -0500

    mongo db added and cows,goat database implemented

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const postsData = require('./posts.json');
-const needPost = require('./needsPost.json');
-const sellerDb = require('./sellerDb.json')
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://farmmart:5msudjZZgqYC06AT@myfirstdb.w4kvmll.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const all_animals = client.db('farmmart').collection('all_animals')
+        app.get('/all_animals', async(req,res)=>{
+            const query = { }
+            const result = await all_animals.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/three_cow', async(req,res)=>{
+            const query = { animal : 'cow'}
+            const result = await all_animals.find(query).limit(3).toArray()
+            res.send(result)
+        })
+        app.get('/all_cow', async(req,res)=>{
+            const query = { animal : 'cow'}
+            const result = await all_animals.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/three_goat', async(req,res)=>{
+            const query = { animal : 'Goat'}
+            const result = await all_animals.find(query).limit(3).toArray()
+            res.send(result)
+        })
+        app.get('/all_goat', async(req,res)=>{
+            const query = { animal : 'Goat'}
+            const result = await all_animals.find(query).toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+
+run().catch(console.log)
+
 app.use(cors())
 app.get('/', (req,res)=>{
     res.send("api is running")
 })
 
-app.get('/posts', (req,res)=>{
-    res.send(postsData)
-})
 
-app.get('/needPosts', (req,res)=>{
-    res.send(needPost)
-})
-
-app.get('/sellersDb', (req,res)=>{
-    res.send(sellerDb)
-})
 
 app.listen(port, ()=>{
     console.log(`api is running on port ${port}`)
