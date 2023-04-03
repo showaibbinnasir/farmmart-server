06df45333a483180c6d67611a3fb7c32fb39e571
showaibbinnasir <showaibnishad@gmail.com>
2023-04-03 06:30:50 -0500

    added all route

--- index.js
+++ index.js
@@ -12,22 +12,22 @@ const uri = "mongodb+srv://farmmart:5msudjZZgqYC06AT@myfirstdb.w4kvmll.mongodb.n
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
+    try {
         const all_animals = client.db('farmmart').collection('all_animals')
         const all_needs = client.db('farmmart').collection('all_needs')
         const all_users = client.db('farmmart').collection('users')
         const all_orders = client.db('farmmart').collection('orders')
-        app.get('/all_animals', async(req,res)=>{
+        app.get('/all_animals', async (req, res) => {
             const searchId = req.query.searchId;
 
-            let query = { }
-            if(searchId){
-                query = {title: {$regex:searchId, $options: '$i'}}
+            let query = {}
+            if (searchId) {
+                query = { title: { $regex: searchId, $options: '$i' } }
             }
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        
+
 
         // app.get('/animals/:id', async(req,res)=>{
         //     const searchData = req.params.id;
@@ -35,148 +35,188 @@ async function run() {
         //     res.send(result)
         // })
 
-        app.get('/three_cow', async(req,res)=>{
-            const query = { animal : 'Cow'}
+        app.get('/three_cow', async (req, res) => {
+            const query = { animal: 'Cow' }
             const result = await all_animals.find(query).limit(3).toArray()
             res.send(result)
         })
-        app.get('/all_cow', async(req,res)=>{
-            const query = { animal : 'Cow'}
+        app.get('/all_cow', async (req, res) => {
+            const query = { animal: 'Cow' }
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        app.get('/three_goat', async(req,res)=>{
-            const query = { animal : 'Goat'}
+        app.get('/three_goat', async (req, res) => {
+            const query = { animal: 'Goat' }
             const result = await all_animals.find(query).limit(3).toArray()
             res.send(result)
         })
-        app.get('/all_goat', async(req,res)=>{
-            const query = { animal : 'Goat'}
+        app.get('/all_goat', async (req, res) => {
+            const query = { animal: 'Goat' }
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        app.get('/three_duck', async(req,res)=>{
-            const query = { animal : 'duck'}
+        app.get('/three_duck', async (req, res) => {
+            const query = { animal: 'duck' }
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        app.get('/three_needs', async(req,res)=>{
+        app.get('/three_needs', async (req, res) => {
             const data = req.query.animal;
             let query = {}
-            if(data) {
+            if (data) {
                 query = {
-                    animal : data
+                    animal: data
                 }
             }
             const result = await all_needs.find(query).toArray();
             res.send(result)
         })
-        app.get('/product/:id', async(req,res)=>{
+        app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id : new ObjectId(id) }
-            
+            const query = { _id: new ObjectId(id) }
+
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        app.get('/needsproduct/:id', async(req,res)=>{
+        app.get('/needsproduct/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id : new ObjectId(id) }
-            
+            const query = { _id: new ObjectId(id) }
+
             const result = await all_needs.find(query).toArray()
             res.send(result)
         })
 
-        app.get('/all_needs', async(req,res)=>{
+        app.get('/all_needs', async (req, res) => {
             const searchId = req.query.searchId;
 
-            let query = { }
-            if(searchId){
-                query = {title: {$regex:searchId, $options: '$i'}}
+            let query = {}
+            if (searchId) {
+                query = { title: { $regex: searchId, $options: '$i' } }
             }
             const result = await all_needs.find(query).toArray()
             res.send(result)
         })
-        app.get('/all_needstwo', async(req,res)=>{
+        app.get('/all_needstwo', async (req, res) => {
             const searchEmail = req.query.email;
 
-            let query = { }
-            if(searchEmail){
-                query = {seller_Email : searchEmail}
+            let query = {}
+            if (searchEmail) {
+                query = { seller_Email: searchEmail }
             }
             const result = await all_needs.find(query).toArray()
             res.send(result)
         })
-        app.get('/all_animalstwo', async(req,res)=>{
+        app.get('/all_animalstwo', async (req, res) => {
             const searchEmail = req.query.email;
 
-            let query = { }
-            if(searchEmail){
-                query = {sellerEmail : searchEmail}
+            let query = {}
+            if (searchEmail) {
+                query = { sellerEmail: searchEmail }
             }
             const result = await all_animals.find(query).toArray()
             res.send(result)
         })
-        
-        app.get('/all_users', async(req,res)=>{
+
+        app.get('/all_users', async (req, res) => {
             const email = req.query.email;
             let query = {}
-            if(email){
-                query = {userEmail : email}
+            if (email) {
+                query = { userEmail: email }
             }
             const result = await all_users.find(query).toArray()
             res.send(result)
         })
-        app.get('/orders', async(req,res)=>{
+        app.get('/orders', async (req, res) => {
             const email = req.query.email;
             let query = {}
-            if(email){
-                query = {buyerEmail : email}
+            if (email) {
+                query = { buyerEmail: email }
             }
             const result = await all_orders.find(query).toArray()
             res.send(result)
         })
 
-        app.post('/all_users', async(req,res)=>{
+        app.post('/all_users', async (req, res) => {
             const newUser = req.body;
             const result = await all_users.insertOne(newUser)
             res.send(result)
         })
 
-        app.post('/all_animals', async(req,res)=>{
+        app.post('/all_animals', async (req, res) => {
             const newPost = req.body;
             const result = await all_animals.insertOne(newPost)
             res.send(result)
         })
-        app.post('/all_needs', async(req,res)=>{
+        app.post('/all_needs', async (req, res) => {
             const newPost = req.body;
             const result = await all_needs.insertOne(newPost)
             res.send(result)
         })
-        app.post('/orders', async(req,res)=>{
+        app.post('/orders', async (req, res) => {
             const newOrders = req.body;
             const result = await all_orders.insertOne(newOrders)
             res.send(result)
         })
 
-        app.delete('/all_needs/:id', async(req,res)=> {
-            
-                const data = req.params.id;
-                const query = { _id: new ObjectId(data) };
-                const result = await all_needs.deleteOne(query);
-                res.send(result)
-            
+        app.put('/all_users/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const user = req.body;
+            const option = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    status: user.verification,
+                }
+            }
+            const result = await all_users.updateOne(filter, updatedUser, option);
+            res.send(result);
         })
-        app.delete('/all_animals/:id', async(req,res)=> {
-            
+        app.put('/all_needs/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const user = req.body;
+            const option = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    status: user.verification,
+                }
+            }
+            const result = await all_needs.updateOne(filter, updatedUser, option);
+            res.send(result);
+        })
+        app.put('/all_animals/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const user = req.body;
+            const option = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    status: user.verification,
+                }
+            }
+            const result = await all_animals.updateOne(filter, updatedUser, option);
+            res.send(result);
+        })
+
+        app.delete('/all_needs/:id', async (req, res) => {
+
+            const data = req.params.id;
+            const query = { _id: new ObjectId(data) };
+            const result = await all_needs.deleteOne(query);
+            res.send(result)
+
+        })
+        app.delete('/all_animals/:id', async (req, res) => {
+
             const data = req.params.id;
             const query = { _id: new ObjectId(data) };
             const result = await all_animals.deleteOne(query);
             res.send(result)
-        
-    })
-        
+
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
@@ -185,13 +225,13 @@ async function run() {
 run().catch(console.log)
 
 app.use(cors())
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("api is running")
 })
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`api is running on port ${port}`)
 })
 
